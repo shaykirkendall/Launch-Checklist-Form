741103b69b0e4b026f2572f22f0c4c1f10cfb6f7
Shay <shay.kirkendall@gmail.com>
2020-09-14 20:28:38 -0500

    Complete

--- script.js
+++ script.js
@@ -77,6 +77,26 @@
          }
       });
    });
+
+   function retrieveData() {
+
+      fetch("https://handlers.education.launchcode.org/static/planets.json").then( function (response) {
+         response.json().then( function(json) {
+            let div = document.getElementById("missionTarget");
+            div.innerHTML = `
+               <h2>Mission Destination</h2>
+                  <ol>
+                     <li>Name: ${json[0].name}</li>
+                     <li>Diameter: ${json[0].diameter}</li>
+                     <li>Star: ${json[0].star}</li>
+                     <li>Distance from Earth: ${json[0].distance}</li>
+                     <li>Number of Moons: ${json[0].moons}</li>
+                  </ol>
+                  <img src="${json[0].image}"></img>
+               `;
+         });
+      });
+   }
 //});
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
