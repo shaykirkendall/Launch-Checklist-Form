27d1b8003652ba7be37b4a62d68bc92a648af20e
Shay <shay.kirkendall@gmail.com>
2020-09-14 19:16:06 -0500

    commit 1

--- script.js
+++ script.js
@@ -1,5 +1,83 @@
 // Write your JavaScript code here!
 
+ window.addEventListener("load", function() {
+   let form = document.querySelector("form");
+
+   form.addEventListener("submit", function(event) {
+      event.preventDefault();
+      
+      let items = document.getElementById("faultyItems");
+      let launchStatus = document.getElementById("launchStatus");
+      let fuelStatus = document.getElementById("fuelStatus");
+      let cargoStatus = document.getElementById("cargoStatus");
+      let ready = true;
+      //let pilotStatus = document.getElementById("pilotStatus");
+      //let cargoStatus =  document.getElementById("copilotStatus");
+
+      let pilotName = document.querySelector("input[name=pilotName]").value;
+		let copilotName = document.querySelector("input[name=copilotName]").value;
+		let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
+		let cargoMass = document.querySelector("input[name=cargoMass]").value;
+      
+
+      if (pilotName === null || pilotName === "") {
+         alert("All fields are requried!");
+         //return false;
+         //event.preventDefault();
+      } if (copilotName === null || copilotName === "") {
+         alert("All fields are required!");
+         //return false;
+         //event.preventDefault();
+      } if (isNaN(fuelLevel) || fuelLevel === "") {
+         alert("Make sure to enter valid information for each field!");
+         //return false;
+         //event.preventDefault();
+      } if (isNaN(cargoMass) || cargoMass === "") {
+         alert("Make sure to enter valid information for each field!");
+         //return false;
+         //event.preventDefault();
+      
+      } else {
+   
+
+      items.style.visibility = "visible";
+      
+      document.getElementById("pilotStatus").innerHTML = `Pilot  ${ pilotName}  Ready`;
+      
+      document.getElementById("copilotStatus").innerHTML = `Co-pilot ${ copilotName + " " }Ready`;
+
+      if (fuelLevel < 10000) {
+         ready = false;
+         fuelStatus.innerHTML = "Fuel level too low for launch";
+         //event.preventDefault();
+      } else {
+         fuelStatus.innerHTML = "Fuel level high enough for launch";
+         //event.preventDefault();
+      }
+
+      if (cargoMass > 10000 ) {
+         ready = false;
+         cargoStatus.innerHTML = "Cargo mass too high for launch";
+         //event.preventDefault();
+      } else {
+         cargoStatus.innerHTML = "Cargo mass low enough for launch";
+         //event.preventDefault();
+      }
+
+      if (ready) {
+         launchStatus.style.color = "green";
+         launchStatus.innerHTML = "Shuttle is ready for launch";
+         retrieveData();
+      } else {
+         items.style.visibility = "visible";
+         launchStatus.style.color = "red";
+         launchStatus.innerHTML = "Shuttle not ready for launch";
+            }
+
+         }
+      });
+   });
+//});
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
